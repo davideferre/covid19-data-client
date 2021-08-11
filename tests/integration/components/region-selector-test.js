cb3e155056aee18c69f50df2df77b1209a773df8
Davide Ferrero <davideferre@gmail.com>
2021-08-11 16:17:40 +0200

    [WIP] Add region selector test

--- tests/integration/components/region-selector-test.js
+++ tests/integration/components/region-selector-test.js
@@ -6,21 +6,12 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | region-selector', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
+  test('it renders italian regions selector', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
     await render(hbs`<RegionSelector />`);
 
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      <RegionSelector>
-        template block text
-      </RegionSelector>
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom('[data-test-region-option]').exists({ count: 22 });
   });
 });
