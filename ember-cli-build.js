504c3d5eff82082f64bb969d69c1f77a6f1025ef
Davide Ferrero <davideferre@gmail.com>
2021-10-09 15:07:46 +0200

    [WIP] Replacing Bootstrap with Tailwind

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,11 +2,35 @@
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
+const isProduction = EmberApp.env() === 'production';
+
+const purgeCSS = {
+  module: require('@fullhuman/postcss-purgecss'),
+  options: {
+    content: [
+      // add extra paths here for components/controllers which include tailwind classes
+      './app/index.html',
+      './app/templates/**/*.hbs',
+    ],
+    defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+  },
+};
+
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
-    sassOptions: {
-      includePaths: ['node_modules/bootstrap/scss'],
+    postcssOptions: {
+      compile: {
+        plugins: [
+          {
+            module: require('postcss-import'),
+            options: {
+              path: ['node_modules'],
+            },
+          },
+          require('tailwindcss')('./app/tailwind/config.js'),
+          ...(isProduction ? [purgeCSS] : []),
+        ],
+      },
     },
   });
 
@@ -31,7 +55,6 @@ module.exports = function (defaults) {
       using: [{ transformation: 'amd', as: 'chartjs-adapter-date-fns' }],
     }
   );
-  app.import('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js');
 
   return app.toTree();
 };
