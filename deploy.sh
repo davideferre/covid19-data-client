c5b3ff6b9f432f41ea67464e8b9c876a37f0511a
Davide Ferrero <davideferre@gmail.com>
2021-11-06 15:08:06 +0100

    Fixes deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,9 @@ deploy() {
   ember build --environment production
   tar -czvf ${FILE_NAME} dist/
   scp -P${SSH_PORT} -i ${SSH_KEY_PATH} ${FILE_NAME} ${SSH_USER}@${SSH_HOST}:${SSH_DEST_FOLDER}
-  ssh -p${SSH_PORT} -i ${SSH_KEY_PATH} ${SSH_USER}@${SSH_HOST} "cd ${SSH_DEST_FOLDER} && mkdir -p ${RELEASE_FOLDER_NAME} && cd ${RELEASE_FOLDER_NAME}\\
-  && tar -xzf ${SSH_DEST_FOLDER}/${FILE_NAME} && mv dist ${APP_NAME} && rm -rf ${SSH_DEST_FOLDER}/${FILE_NAME}"
+  ssh -p${SSH_PORT} -i ${SSH_KEY_PATH} ${SSH_USER}@${SSH_HOST} "cd ${SSH_DEST_FOLDER} && tar -xzf ${SSH_DEST_FOLDER}/${FILE_NAME} && mv dist ${SSH_DEST_FOLDER}/releases/${RELEASE_FOLDER_NAME} && rm -rf ${SSH_DEST_FOLDER}/${FILE_NAME}"
   rm ${FILE_NAME}
-  echo -e "Deployed \e[93m${FILE_NAME} \e[39mto \e[31m${SSH_HOST}:${SSH_PORT} \e[39min \e[96m${SSH_DEST_FOLDER} \e[39mfolder."
+  echo -e "Deployed \033[93m${FILE_NAME} \033[39mto \033[31m${SSH_HOST}:${SSH_PORT} \033[39min \033[96m${SSH_DEST_FOLDER} \033[39mfolder."
 }
 
 activate() {
@@ -16,18 +15,18 @@ activate() {
       echo "No revision"
       exit 1
   fi
-  echo -e "Activating \e[93m$1 \e[39mrevision..."
+  echo -e "Activating \033[93m$1 \033[39mrevision..."
   ssh -p${SSH_PORT} -i ${SSH_KEY_PATH} ${SSH_USER}@${SSH_HOST} "unlink ${SSH_DEST_FOLDER}/current && ln -s ${SSH_DEST_FOLDER}/releases/$1 ${SSH_DEST_FOLDER}/current"
-  echo -e "Revision \e[96m$1 \e[39mactivated."
+  echo -e "Revision \033[96m$1 \033[39mactivated."
 }
 
 list() {
-  echo -e "Listing \e[96m${SSH_DEST_FOLDER} \e[39mfolder..."
+  echo -e "Listing \033[96m${SSH_DEST_FOLDER} \033[39mfolder..."
   ssh -p${SSH_PORT} -i ${SSH_KEY_PATH} ${SSH_USER}@${SSH_HOST} "ls -lha ${SSH_DEST_FOLDER}/releases"
   RELEASED_REVISION_PATH=`ssh -p${SSH_PORT} -i ${SSH_KEY_PATH} ${SSH_USER}@${SSH_HOST} "readlink -f ${SSH_DEST_FOLDER}/current"`
   RELEASED_REVISION=${RELEASED_REVISION_PATH#${SSH_DEST_FOLDER}}
-  echo -e "Released revision is \e[93m${RELEASED_REVISION}\e[39m"
-  echo -e "Listing \e[96m${SSH_DEST_FOLDER} \e[39mfolder DONE."
+  echo -e "Released revision is \033[93m${RELEASED_REVISION}\033[39m"
+  echo -e "Listing \033[96m${SSH_DEST_FOLDER} \033[39mfolder DONE."
 }
 
 remove() {
@@ -36,16 +35,16 @@ remove() {
       echo "No revision"
       exit 1
   fi
-  RELEASED_REVISION_PATH=`ssh -p${SSH_PORT} -i ${SSH_KEY_PATH} ${SSH_USER}@${SSH_HOST} "readlink -f ${SSH_DEST_FOLDER}/release"`
-  RELEASED_REVISION=${RELEASED_REVISION_PATH#${SSH_DEST_FOLDER}}
+  RELEASED_REVISION_PATH=`ssh -p${SSH_PORT} -i ${SSH_KEY_PATH} ${SSH_USER}@${SSH_HOST} "readlink -f ${SSH_DEST_FOLDER}/current"`
+  RELEASED_REVISION=${RELEASED_REVISION_PATH#"${SSH_DEST_FOLDER}/releases/"}
   if [ "$1" == "$RELEASED_REVISION" ]
     then
-      echo -e "Can't remove released version \e[31m$1\e[39m"
+      echo -e "Can't remove released version \033[31m$1\033[39m"
       exit 1
   fi
-  echo -e "Removing \e[93m$1 \e[39mrevision..."
-  ssh -p${SSH_PORT} -i ${SSH_KEY_PATH} ${SSH_USER}@${SSH_HOST} "rm -rf ${SSH_DEST_FOLDER}/$1"
-  echo -e "Revision \e[96m$1 \e[39mremoved."
+  echo -e "Removing \033[93m$1 \033[39mrevision..."
+  ssh -p${SSH_PORT} -i ${SSH_KEY_PATH} ${SSH_USER}@${SSH_HOST} "rm -rf ${SSH_DEST_FOLDER}/releases/$1"
+  echo -e "Revision \033[96m$1 \033[39mremoved."
 }
 
 DEPLOY_ENV_FILE='.env.deploy.production'
