6702b5e19422c55fbbe30febb5e0f3d922676b22
Davide Ferrero <davideferre@gmail.com>
2021-12-02 22:44:48 +0100

    Adds addon for PWA

---
+++ config/manifest.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = function(/* environment, appConfig */) {
+  // See https://zonkyio.github.io/ember-web-app for a list of
+  // supported properties
+
+  return {
+    name: 'Dati COVID 19 Italia',
+    short_name: 'COVID19 Italian data',
+    description: 'Riepilogo dei dati COVID 19 in Italia',
+    start_url: '/',
+    scope: '/',
+    display: 'fullscreen',
+    orientation: 'portrait',
+    lang: 'it-IT',
+    background_color: '#fff',
+    theme_color: '#fff',
+    icons: [
+      {
+        sizes: '512x512',
+        src: '/assets/icons/maskable_icon_x512.png',
+        type: 'image/png',
+      },
+      {
+        purpose: 'maskable',
+        sizes: '512x512',
+        src: '/assets/icons/maskable_icon_x512.png',
+        type: 'image/png',
+      },
+    ],
+    ms: {
+      tileColor: '#fff',
+    },
+  };
+};
