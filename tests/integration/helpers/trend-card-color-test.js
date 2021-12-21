808bb448cb7f298ffe9cc75c4a78e334375f9c07
Davide Ferrero <davideferre@gmail.com>
2021-12-21 22:37:04 +0100

    Fixes tests
    Please enter the commit message for your changes. Lines starting

--- tests/integration/helpers/trend-card-color-test.js
+++ tests/integration/helpers/trend-card-color-test.js
@@ -6,57 +6,57 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Helper | trend-card-color', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders bg-yellow-500 with greater set to true', async function (assert) {
+  test('it renders bg-yellow-700 with greater set to true', async function (assert) {
     this.set('trend', 0);
     this.set('isGreater', true);
 
     await render(hbs`{{trend-card-color this.trend isGreater=this.isGreater}}`);
 
-    assert.dom(this.element).hasText('bg-yellow-500');
+    assert.dom(this.element).hasText('bg-yellow-700');
   });
 
-  test('it renders bg-green-500 with greater set to true', async function (assert) {
+  test('it renders bg-green-700 with greater set to true', async function (assert) {
     this.set('trend', 1);
     this.set('isGreater', true);
 
     await render(hbs`{{trend-card-color this.trend isGreater=this.isGreater}}`);
 
-    assert.dom(this.element).hasText('bg-green-500');
+    assert.dom(this.element).hasText('bg-green-700');
   });
 
-  test('it renders bg-red-500 with greater set to true', async function (assert) {
+  test('it renders bg-red-700 with greater set to true', async function (assert) {
     this.set('trend', -1);
     this.set('isGreater', true);
 
     await render(hbs`{{trend-card-color this.trend isGreater=this.isGreater}}`);
 
-    assert.dom(this.element).hasText('bg-red-500');
+    assert.dom(this.element).hasText('bg-red-700');
   });
 
-  test('it renders bg-yellow-500 with greater set to false', async function (assert) {
+  test('it renders bg-yellow-700 with greater set to false', async function (assert) {
     this.set('trend', 0);
     this.set('isGreater', false);
 
     await render(hbs`{{trend-card-color this.trend isGreater=this.isGreater}}`);
 
-    assert.dom(this.element).hasText('bg-yellow-500');
+    assert.dom(this.element).hasText('bg-yellow-700');
   });
 
-  test('it renders bg-green-500 with greater set to false', async function (assert) {
+  test('it renders bg-green-700 with greater set to false', async function (assert) {
     this.set('trend', -1);
     this.set('isGreater', false);
 
     await render(hbs`{{trend-card-color this.trend isGreater=this.isGreater}}`);
 
-    assert.dom(this.element).hasText('bg-green-500');
+    assert.dom(this.element).hasText('bg-green-700');
   });
 
-  test('it renders bg-red-500 with greater set to false', async function (assert) {
+  test('it renders bg-red-700 with greater set to false', async function (assert) {
     this.set('trend', 1);
     this.set('isGreater', false);
 
     await render(hbs`{{trend-card-color this.trend isGreater=this.isGreater}}`);
 
-    assert.dom(this.element).hasText('bg-red-500');
+    assert.dom(this.element).hasText('bg-red-700');
   });
 });
